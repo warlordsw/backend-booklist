c5848ae50cc96311e7886b8b0b3f252c2fc981c7
Caner Kuncu <warlordsw@gmail.com>
2021-05-21 21:15:09 +0300

    First commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import bookRoutes from './routes/book.js'
 import cors from 'cors'
 const app = express()
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(
+  cors({ origin: 'https://booklist-project.herokuapp.com/', credentials: true })
+)
 
 app.use(express.json({ extended: false }))
 
